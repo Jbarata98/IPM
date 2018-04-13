e34ffc472dc9728176cc1e0841b038b079975880
João Barata <jbarata1998@gmail.com>
2018-04-13 15:50:01 +0100

    slideshow

---
+++ interactions.js
@@ -0,0 +1,20 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    
+    document.getElementById('horas').innerHTML =
+    h + ":" + m ;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+function show(shown, hidden) {
+    document.getElementById(shown).style.display='block';
+    document.getElementById(hidden).style.display='none';
+    return false;
+  }
